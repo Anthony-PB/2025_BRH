2fd56365ad272520ebc4183d4e1436ff06fcaec1
Anthony Paredes-Bautista <anthonyparedesb0@gmail.com>
2025-09-20 19:11:51 -0400

    views for source and articles kind of

---
+++ backend/aggregator/serializers.py
@@ -0,0 +1,20 @@
+from rest_framework import serializers
+from .models import Source, Article
+
+class SourceSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Source
+        fields = ['id', 'name', 'base_url', 'feed_url', 'category', 'is_rss'] 
+        
+    def to_representation(self, instance):
+        data = super().to_representation(instance)
+        data['last_scraped'] = instance.last_scraped
+        return data
+
+class ArticleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Article
+        fields = [
+            'id', 'title', 'summary', 'author', 'original_url', 
+            'published_at', 'scraped_at', 'source', 'tags'
+        ]
