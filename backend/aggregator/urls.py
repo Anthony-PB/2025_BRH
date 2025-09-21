be1727ca44e9abfae6c897032e7f6a4357535967
BryanKudi <bryankudiabor123@gmail.com>
2025-09-20 21:01:13 -0400

    Test cases for views.py

--- backend/aggregator/urls.py
+++ backend/aggregator/urls.py
@@ -2,8 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="aggregator-index"),
-    # Add more URLs as you build features:
-    # path("articles/", views.article_list, name="article-list"),
-    # path("sources/", views.source_list, name="source-list"),
+    path('sources/', views.SourceView.as_view(), name='source-create'),
+    # Add more endpoints as needed:
+    # path("articles/", views.ArticleView.as_view(), name="article-list"),
 ]
